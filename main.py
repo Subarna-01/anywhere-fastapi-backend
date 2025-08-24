b2efe3dbd3d4b3c599fbb8a13b2c4e3bfe473369
Subarna-01 <subarnabhowmik01@gmail.com>
2025-08-24 23:51:42 +0530

    Added root route

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from modules.mail.router import mail_router
 
 app = FastAPI(title='Anywhere FastAPI APIs v1.0.0')
 
 origins = ['*']
+
+@app.get('/')
+async def get_root() -> dict:
+    return {
+        'msg': 'Hello World',
+        'status_code': status.HTTP_200_OK
+    }
 
 app.add_middleware(
     CORSMiddleware,
